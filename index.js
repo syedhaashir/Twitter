f310a6fe50d355e42a284699733310caaaa74106
Hashir Ali <98033127+syedhaashir@users.noreply.github.com>
2022-09-16 23:07:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function Hi(props) {
     <div className='header'>
         <h1>Twitter</h1>
     </div>
+  
+  
     <div className='post'>
         <div className='post-header'>
           <img className='profilePhoto' src={props.pfp} alt="Profile Picture"/> 
